67dc9ef9c101920c2832658dc44b109b2db8bb9e
Thierno alioune gueye <tagartiste@gmail.com>
2026-02-03 18:47:49 +0000

    Fix migration BOM causing Postgres syntax error

--- backend/prisma/migrations/20260203160355_institutional_hierarchy/migration.sql
+++ backend/prisma/migrations/20260203160355_institutional_hierarchy/migration.sql
@@ -1,4 +1,4 @@
-﻿-- AlterEnum
+-- AlterEnum
 -- This migration adds more than one value to an enum.
 -- With PostgreSQL versions 11 and earlier, this is not possible
 -- in a single migration. This can be worked around by creating
